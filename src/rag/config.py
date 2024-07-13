715ba57b74c20bcd39ce55ef7c30eb6f4ced41cd
emVisible <1619346963@qq.com>
2024-07-13 17:30:10 +0800

    feat: fix structure

--- src/rag/config.py
+++ src/rag/config.py
@@ -1,6 +1,5 @@
 from os import getenv
-from os.path import join,abspath
-
+from os.path import dirname, join, abspath
 from dotenv import load_dotenv
 
 env_path = join(abspath("./"), ".env")
@@ -12,6 +11,7 @@ class PathConfig:
     DOC_ADDR = getenv("DOC_ADDR", "docs")
     DB_ADDR = getenv("DB_ADDR", "db_vector")
 
+
 class RAGConfig:
     EMBEDDING_MODEL_PATH = getenv(
         "EMBEDDING_PATH", "shibing624_text2vec-base-chinese-paraphrase"
@@ -19,3 +19,19 @@ class RAGConfig:
     CHUNK_SIZE = 500
     CHUNK_OVERLAP = 50
     QUERY_QUANTITY = 10
+
+
+def log_msg():
+    max_length = 200
+    print(f"[RAG-CONFIG] root path: {env_path}".ljust(max_length))
+    print(f"[RAG-CONFIG] llm model path: {PathConfig.MODEL_PATH}".ljust(max_length))
+    print(
+        f"[RAG-CONFIG] embedding model path: {RAGConfig.EMBEDDING_MODEL_PATH}".ljust(
+            max_length
+        )
+    )
+    print(f"[RAG-CONFIG] doc path: {PathConfig.DOC_ADDR}".ljust(max_length))
+    print(f"[RAG-CONFIG] db path: {PathConfig.DB_ADDR}".ljust(max_length))
+
+
+log_msg()
